e28a6cbf8c353cec314e44630efebf017b8a301a
Kyle Lawrence <alawrence815@gmail.com>
2024-04-14 20:34:36 -0400

    Test change

--- SIN_Wave.py
+++ SIN_Wave.py
@@ -2,6 +2,6 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 x = np.arange(400)
-y = 400 * (np.sin(0.5*np.pi*5*(x/200)))
+y = 100 * (np.sin(0.5*np.pi*5*(x/200)))
 plt.plot(x, y)
 plt.show()
